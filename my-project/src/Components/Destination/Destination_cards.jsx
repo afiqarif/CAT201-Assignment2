fdb946ef6789e1f5b481c10844883c1e251f761b
afiqarif <akamarulzaidi@gmail.com>
2024-12-30 22:06:08 +0800

    Edit Destination page

--- my-project/src/Components/Destination/Destination_cards.jsx
+++ my-project/src/Components/Destination/Destination_cards.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Cards = (props) => {
+
   return (
-    <div className='py-12 bg-gray-200'>
-      <div className='container mx-auto px-28'>
-          <h3 className='text-3xl font-bold text-center'>
-            {props.title}
-          </h3>
-          <p className='text-center mb-8'>
-            {props.desc}
-          </p>
-          <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-              {props.arr.map((place, index) => (
-                <div key={index} className='bg-white rounded-lg shadow-md overflow-hidden cursor-pointer'>
-                  <img src={place.image} alt={place.title} className='w-full h-48 object-cover
-                  transform transition duration-300 hover:scale-110'/>
-                  <div className='p-4'>
-                    <h4 className='text-xl font-bold mb-2 text-center'>{place.title}</h4>
-                    <p className='text-gray-600 text-justify line-clamp-3'>{place.description}</p>
-                  </div>
-                </div>
-            ))}
+    <>
+      <div className='py-12 bg-gray-200'>
+        <div className='container mx-auto px-28'>
+            <h3 className='text-3xl font-bold text-center'>
+              {props.title}
+            </h3>
+            <p className='text-center mb-8'>
+              {props.desc}
+            </p>
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {props.arr.map(
+                  (place, index) => (
+                    <Link
+                      to="/detail_page"
+                      key={index}
+                      className='bg-white rounded-lg shadow-md overflow-hidden cursor-pointer'
+                      state={{
+                        image: place.image,
+                        title: place.title,
+                        description: place.description
+                      }}>
+                        <img src={place.image} alt={place.title} className='w-full h-48 object-cover
+                        transform transition duration-300 hover:scale-110'/>
+                        <div className='p-4'>
+                          <h4 className='text-xl font-bold mb-2 text-center'>{place.title}</h4>
+                          <p className='text-gray-600 text-justify line-clamp-3'>{place.description}</p>
+                        </div>
+                    </Link>
+                  )
+                )
+              }
+            </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- my-project/src/Components/Destination/destination_info.js
+++ my-project/src/Components/Destination/destination_info.js
@@ -1,76 +1,76 @@
 //This JavaScript file stored the information array for destination.
 
 export const Outdoor_Arr = [
-    {
-      image: '/image/penang-hill.jpg',
-      title: 'Penang Hill',
-      description: 'Penang Hill is a lush retreat above George Town, offering cool air, stunning views, and rich biodiversity. Ride the funicular to explore colonial-era charm, vibrant gardens, and the iconic Skywalk for panoramic vistas.'
-    },
-    {
-      image: '/image/penang-botanic-garden.jpg',
-      title: 'Penang Botanic Garden',
-      description: 'The Penang Botanic Gardens is a botanical garden within the city of George Town in the Malaysian state of Penang. Situated at Jalan Air Terjun, it is also colloquially known as the Waterfall Gardens because of the cascading waterfall nearby.'
-    },
-    {
-      image: '/image/fruit-farm.jpg',
-      title: 'Tropical Fruit Farm',
-      description: 'Penang Tropical Fruit Farm is a vibrant, 25-acre orchard offering a unique taste of Malaysia\'s tropical bounty. Visitors can tour the farm, sample exotic fruits, and enjoy stunning views of the island\'s natural beauty.'
-    },
-    {
-      image: '/image/jerejak-island.jpg',
-      title: 'Jerejak Island',
-      description: 'Jerejak Island is a 3.62 km² islet off the eastern coast of Penang Island in the Malaysian state of Penang. It is also a short ferry ride from Bayan Lepas, a suburb of George Town. It was formerly the main leper asylum for the Straits Settlements, a Quarantine Station and a penal colony.'
-    }
-  ]
-  
-  export const Historical_Arr = [
-    {
-      image: '/image/snake-temple.jpg',
-      title: 'Snake Temple',
-      description: 'The Snake Temple, also known as the Temple of the Azure Clouds, is a Chinese temple within George Town in the Malaysian state of Penang.'
-    }, 
-    {
-      image: '/image/kek-lok-si-temple.jpg',
-      title: 'Kek Lok Si Temple, Air Hitam',
-      description: 'The Kek Lok Si Temple is a Buddhist temple within the city of George Town in the Malaysian state of Penang. Located at Ayer Itam, it is the largest Buddhist temple in Malaysia and an important pilgrimage centre for Buddhists from Hong Kong, the Philippines, Singapore and other parts of Southeast Asia.'
-    }, 
-    {
-      image: 'image/st-george-church.jpg',
-      title: 'St. George\'s Anglican Church',
-      description: 'St. George\'s Church is a 19th-century Anglican church within George Town in the Malaysian state of Penang. The oldest purpose-built Anglican church in Southeast Asia, it was elevated by to the status of pro-cathedral in 2023.'
-    }, 
-    {
-      image: '/image/burmese-temple.jpg',
-      title: 'Dhammikarama Burmese Temple',
-      description: 'Dhammikarama Burmese Temple is a Theravada Buddhist temple within George Town in the Malaysian state of Penang.'
-    }
-  ]
-  
-  export const Amusement_Arr = [
-    {
-      image: '/image/escape-penang.jpg',
-      title: 'ESCAPE Penang',
-      description: 'Escape Penang is an exciting adventure park nestled in the jungle, offering a variety of thrilling outdoor activities. From zip-lining to obstacle courses, it\'s the perfect place for adrenaline seekers and nature lovers alike, blending fun with breathtaking scenery.'
-    },
-    {
-      image: '/image/the-top.jpg',
-      title: 'The TOP Penang',
-      description: 'The TOP Penang is a dynamic entertainment hub located in George Town, featuring thrilling attractions like the Rainbow Skywalk, an indoor theme park, and the Sky Tower with stunning panoramic views. It\'s a perfect blend of excitement, shopping, and dining for all ages.'
-    },
-    {
-      image: '/image/bertam-souk.jpg',
-      title: 'Bertam Souk & Water Park',
-      description: 'Escape the ordinary and embark on an exhilarating adventure at Bertam Souk & Water Park, Penang. Plunge down thrilling water slides, ride the waves in a giant wave pool, and unwind in a serene lazy river. With over 14 exhilarating rides and attractions, this Moroccan-themed water park promises a day of unforgettable fun for the whole family.'
-    },
-    {
-      image: '/image/adventure-zone.jpg',
-      title: 'Adventure Zone',
-      description: 'Adventure Zone in Penang is an indoor amusement park packed with fun for all ages. Featuring a range of exciting attractions, from thrilling slides to obstacle courses, it\'s the perfect place for families to enjoy an active, adventurous day indoors.'
-    },
-  ]
-  
-  export const Outdoor_desc = "A scenic escape into nature, offering activities like hiking, picnicking, or simply soaking in breathtaking landscapes and fresh air.";
-  
-  export const Historical_desc = "A glimpse into the past through preserved architecture and artifacts, telling stories of culture, heritage, and significant events.";
-  
-  export const Amusement_desc = "A lively destination with thrilling rides, games, and entertainment, perfect for family fun and creating unforgettable memories.";
+  {
+    image: '/image/penang-hill.jpg',
+    title: 'Penang Hill',
+    description: 'Penang Hill is a lush retreat above George Town, offering cool air, stunning views, and rich biodiversity. Ride the funicular to explore colonial-era charm, vibrant gardens, and the iconic Skywalk for panoramic vistas.'
+  },
+  {
+    image: '/image/penang-botanic-garden.jpg',
+    title: 'Penang Botanic Garden',
+    description: 'The Penang Botanic Gardens is a botanical garden within the city of George Town in the Malaysian state of Penang. Situated at Jalan Air Terjun, it is also colloquially known as the Waterfall Gardens because of the cascading waterfall nearby.'
+  },
+  {
+    image: '/image/fruit-farm.jpg',
+    title: 'Tropical Fruit Farm',
+    description: 'Penang Tropical Fruit Farm is a vibrant, 25-acre orchard offering a unique taste of Malaysia\'s tropical bounty. Visitors can tour the farm, sample exotic fruits, and enjoy stunning views of the island\'s natural beauty.'
+  },
+  {
+    image: '/image/jerejak-island.jpg',
+    title: 'Jerejak Island',
+    description: 'Jerejak Island is a 3.62 km² islet off the eastern coast of Penang Island in the Malaysian state of Penang. It is also a short ferry ride from Bayan Lepas, a suburb of George Town. It was formerly the main leper asylum for the Straits Settlements, a Quarantine Station and a penal colony.'
+  }
+]
+
+export const Historical_Arr = [
+  {
+    image: '/image/snake-temple.jpg',
+    title: 'Snake Temple',
+    description: 'The Snake Temple, also known as the Temple of the Azure Clouds, is a Chinese temple within George Town in the Malaysian state of Penang.'
+  }, 
+  {
+    image: '/image/kek-lok-si-temple.jpg',
+    title: 'Kek Lok Si Temple, Air Hitam',
+    description: 'The Kek Lok Si Temple is a Buddhist temple within the city of George Town in the Malaysian state of Penang. Located at Ayer Itam, it is the largest Buddhist temple in Malaysia and an important pilgrimage centre for Buddhists from Hong Kong, the Philippines, Singapore and other parts of Southeast Asia.'
+  }, 
+  {
+    image: 'image/st-george-church.jpg',
+    title: 'St. George\'s Anglican Church',
+    description: 'St. George\'s Church is a 19th-century Anglican church within George Town in the Malaysian state of Penang. The oldest purpose-built Anglican church in Southeast Asia, it was elevated by to the status of pro-cathedral in 2023.'
+  }, 
+  {
+    image: '/image/burmese-temple.jpg',
+    title: 'Dhammikarama Burmese Temple',
+    description: 'Dhammikarama Burmese Temple is a Theravada Buddhist temple within George Town in the Malaysian state of Penang.'
+  }
+]
+
+export const Amusement_Arr = [
+  {
+    image: '/image/escape-penang.jpg',
+    title: 'ESCAPE Penang',
+    description: 'Escape Penang is an exciting adventure park nestled in the jungle, offering a variety of thrilling outdoor activities. From zip-lining to obstacle courses, it\'s the perfect place for adrenaline seekers and nature lovers alike, blending fun with breathtaking scenery.'
+  },
+  {
+    image: '/image/the-top.jpg',
+    title: 'The TOP Penang',
+    description: 'The TOP Penang is a dynamic entertainment hub located in George Town, featuring thrilling attractions like the Rainbow Skywalk, an indoor theme park, and the Sky Tower with stunning panoramic views. It\'s a perfect blend of excitement, shopping, and dining for all ages.'
+  },
+  {
+    image: '/image/bertam-souk.jpg',
+    title: 'Bertam Souk & Water Park',
+    description: 'Escape the ordinary and embark on an exhilarating adventure at Bertam Souk & Water Park, Penang. Plunge down thrilling water slides, ride the waves in a giant wave pool, and unwind in a serene lazy river. With over 14 exhilarating rides and attractions, this Moroccan-themed water park promises a day of unforgettable fun for the whole family.'
+  },
+  {
+    image: '/image/adventure-zone.jpg',
+    title: 'Adventure Zone',
+    description: 'Adventure Zone in Penang is an indoor amusement park packed with fun for all ages. Featuring a range of exciting attractions, from thrilling slides to obstacle courses, it\'s the perfect place for families to enjoy an active, adventurous day indoors.'
+  },
+]
+
+export const Outdoor_desc = "A scenic escape into nature, offering activities like hiking, picnicking, or simply soaking in breathtaking landscapes and fresh air.";
+
+export const Historical_desc = "A glimpse into the past through preserved architecture and artifacts, telling stories of culture, heritage, and significant events.";
+
+export const Amusement_desc = "A lively destination with thrilling rides, games, and entertainment, perfect for family fun and creating unforgettable memories.";
