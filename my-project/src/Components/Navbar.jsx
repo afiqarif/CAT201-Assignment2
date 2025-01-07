67cf59e8d366e1b0987a1b5b5b62b0de860b0121
afiqarif <akamarulzaidi@gmail.com>
2025-01-07 22:16:35 +0800

    hasfnsakfasfnalkfn

--- my-project/src/Components/Navbar.jsx
+++ my-project/src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar= () => {
                 <h3 className="text-2xl font-bold">Visit Penang</h3>
                 {/* Desktop Menu */}
                 <div className="hidden md:flex space-x-4 text-sm font-bold">
-                    <Link to="/" className="px-6 py-2 hover:bg-gray-600 hover:text-white">
+                    <Link to="/CAT201-Assignment2" className="px-6 py-2 hover:bg-gray-600 hover:text-white">
                         Home
                     </Link>
                     <Link to="/food" className="px-6 py-2 hover:bg-gray-600 hover:text-white">
