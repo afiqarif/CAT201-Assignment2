6a7b729557ab966a2208014d70c46d2dc27d1ac5
afiqarif <akamarulzaidi@gmail.com>
2025-01-08 17:00:07 +0800

    updated

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html', // Path to your 404.html in the project root
+          dest: '.',       // Destination in the dist folder
+        },
+      ],
+    }),
+  ],
   base: "/CAT201-Assignment2/",
 })
